50613c60ee5e940e693043c0d9144ddfcab5f719
gislu <ahlxt@qq.com>
2018-06-21 14:43:48 -0400

    footer  localed

--- src/js/components/pc_footer.js
+++ src/js/components/pc_footer.js
@@ -8,7 +8,7 @@ export default class PCFooter extends React.Component {
 				<Row>
 					<Col span={2}></Col>
 					<Col span={20} class="footer">
-            &copy;&nbsp;2016 ReactNews. All Rights Reserved.
+            &copy;&nbsp;Implemented by Alex Lu in 2018. All Rights Reserved.
 					</Col>
 					<Col span={2}></Col>
 				</Row>
