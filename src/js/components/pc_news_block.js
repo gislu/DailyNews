86c83866640e2268fc9ce2940962ebfd4e04cb1a
gislu <ahlxt@qq.com>
2018-06-27 23:59:54 -0400

    bug fixing...

--- src/js/components/pc_news_block.js
+++ src/js/components/pc_news_block.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Card} from 'antd';
+import {Link} from 'react-router-dom';
 export default class PCNewsBlock extends React.Component {
 	constructor() {
 		super();
@@ -11,30 +12,27 @@ export default class PCNewsBlock extends React.Component {
 		var myFetchOptions = {
 			method: 'GET'
 		};
-		//fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnews&type=" + this.props.type + "&count=" + this.props.count, myFetchOptions).then(response => response.json()).then(json => this.setState({news: json}));
 		let url ="https://newsapi.org/v2/top-headlines?country=us&category="+this.props.type+"&apiKey=8611169f68f94d8582ac555bf7173ec4";
 		fetch(url,myFetchOptions)
 		.then(response => response.json())
 		.then(
 			json => {
-			console.log((typeof(json)))
 			this.setState({news: json.articles})
-			console.log(typeof(this.state.news));
+			console.log(this.state.news);
 		}	
 		);
 	};
 	render() {
-		const {newsapi} = this.state;
-		const newsList ="";
-		// const newsList = newsapi.length
-		// 	? newsapi.articles.map((newsItem, index) => (
-		// 		<li key={index}>
-		// 			<Link to={`details/${newsItem.url}`}>
-		// 				{newsItem.title}
-		// 			</Link>
-		// 		</li>
-		// 	))
-		// 	: 'Loading.....';
+		const {news} = this.state;
+		const newsList = news.length
+			? news.map((newsItem, index) => (
+				<li key={index}>
+					<Link to={`details/${newsItem.url}`}>
+						{newsItem.title}
+					</Link>
+				</li>
+			))
+			: 'Loading.....';
 		return (
 			<div class="topNewsList">
 				<Card>
