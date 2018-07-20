6370a1b7e8c40550df410c2456de281c622f7cee
gislu <ahlxt@qq.com>
2018-07-20 14:22:33 -0400

    change the router

--- src/js/components/mobile_router.js
+++ src/js/components/mobile_router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter,Switch} from 'react-router-dom';
+import {Route, HashRouter,Switch} from 'react-router-dom';
 import MobileNewsDetails from './mobile_news_details';
 import MobileIndex from './mobile_index';
 import MobileUserCenter from './mobile_usercenter';
@@ -7,13 +7,13 @@ import MobileUserCenter from './mobile_usercenter';
 export default class Mobile_Router extends React.Component{
     render(){
         return(
-            <BrowserRouter>
+            <HashRouter>
 						<Switch>
 							<Route exact path="/" component={MobileIndex}></Route>
 							<Route path="/details/:uniquekey" component={MobileNewsDetails}></Route>
 							<Route path="/usercenter" component={MobileUserCenter}></Route>
 						</Switch>
-					</BrowserRouter>
+			</HashRouter>
         );
     }
 }
--- src/js/components/pc_router.js
+++ src/js/components/pc_router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter,Switch} from 'react-router-dom';
+import {Route, HashRouter ,Switch} from 'react-router-dom';
 import PCIndex from './pc_index';
 import PCNewsDetails from './pc_news_details';
 import PCUserCenter from './pc_usercenter';
@@ -9,14 +9,14 @@ import PCVideoDemo from './pc_videodemo';
 export default class PC_Router extends React.Component{    
     render(){
         return(
-            <BrowserRouter>
+            <HashRouter>
 						<Switch>
 							<Route exact path="/" component={PCIndex}></Route>
 							<Route path="/details/:url" component={PCNewsDetails}></Route>
 							<Route path="/usercenter" component={PCUserCenter}></Route>
 							<Route path="/videodemo" component={PCVideoDemo}></Route>
 						</Switch>
-					</BrowserRouter>
+					</HashRouter >
         );
     };
 
